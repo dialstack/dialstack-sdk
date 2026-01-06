8ef713278d57699f85db7a167af72263121aa872
simon-dialstack <simon.perreault@dialstack.ai>
2026-01-06 15:57:29 -0500

    Merge pull request #123 from dialstack/feat/copybara-spineline-publish
    feat(ci): replace git-subtree with Copybara for spineline publishing
    
    GitOrigin-RevId: 2d30013ad17c45914c3aecb72d7ca726662021aa

--- src/setupTests.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Jest setup file - runs before each test
- *
- * This file is excluded from the main build and only used by Jest.
- */
-
-/* eslint-disable @typescript-eslint/no-require-imports */
-
-import '@testing-library/jest-dom';
